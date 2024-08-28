bbe1f43a1b305ab6ff366a45688a19ce81ee607d
Henry <henrywri2020@gmail.com>
2024-08-28 11:30:32 +0200

    add assetpack to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@
 echo "Building the project..."
 npm run build
 
+# run asset pack
+echo "Building assets..."
+npm run assetpack
+
 # Start the application
 echo "Starting the application..."
 exec node ./app/public/dist/server/app/index.js
